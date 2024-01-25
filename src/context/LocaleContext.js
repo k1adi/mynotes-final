16df74605015bc3f6070d76ec39022c99e2ca970
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-01-25 20:35:29 +0700

    create context

---
+++ src/context/LocaleContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const LocaleContext = React.createContext();
+const LocaleContextProvider = LocaleContext.Provider;
+
+export { LocaleContext, LocaleContextProvider };
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ThemeContext = React.createContext();
+const ThemeContextProvider = ThemeContext.Provider;
+
+export { ThemeContext, ThemeContextProvider };
