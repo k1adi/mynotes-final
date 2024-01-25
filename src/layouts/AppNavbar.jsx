b69a0b39626d8ab4ef7888a65aec627d22aef202
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-01-25 20:38:28 +0700

    create navigation in navbar

--- src/layouts/AppNavbar.jsx
+++ src/layouts/AppNavbar.jsx
@@ -1,10 +1,43 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
 
-function AppNavbar() {
+import { AuthContext } from '../context/AuthContext';
+
+function AppNavbar({ logoutHandler }) {
+  const userAuth = React.useContext(AuthContext);
+
+  const location = useLocation();
+  const links = [
+    {path: '/', text: 'Home'},
+    {path: '/note', text: 'Note'},
+    {path: '/archive', text: 'Archive'},
+    {path: '/note/123', text: 'Detail'},
+    {path: '/login', text: 'Login'},
+    {path: '/register', text: 'Register'},
+  ];
+  
   return (
-    <p>Navbar</p>
+    <>
+      <p>Navbar</p>
+      {links.map((link, index) => (
+        <Link key={index} to={link.path} className={location.pathname === link.to ? 'active' : ''}>
+          {link.text}
+        </Link>
+      ))}
+      {userAuth && (
+        <>
+          {userAuth.name}
+          <button onClick={logoutHandler}>Log Out</button>
+        </>
+      )}
+    </>
   );
 }
+
+AppNavbar.propTypes = {
+  logoutHandler: PropTypes.func.isRequired
+};
 
 export default AppNavbar;
