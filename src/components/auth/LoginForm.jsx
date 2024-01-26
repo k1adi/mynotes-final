d4ddba64994b9e408068cdb038b0cea0bd5faddd
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-01-26 11:09:18 +0700

    added button toggle show/hide password

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -7,6 +7,11 @@ import useInput from '../../hooks/useInput';
 function LoginForm({ loginHandler }) {
   const [email, emailChangeHandler] = useInput('');
   const [password, passwordChangeHandler] = useInput('');
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
@@ -17,7 +22,16 @@ function LoginForm({ loginHandler }) {
   return (
     <form onSubmit={onSubmitHandler}>
       <input type="email" value={email} onChange={emailChangeHandler} placeholder="Email" required/>
-      <input type="password" value={password} onChange={passwordChangeHandler} placeholder="Password" required/>
+      <input 
+        type={showPassword ? 'text' : 'password'}
+        value={password} 
+        onChange={passwordChangeHandler} 
+        placeholder="Password" 
+        required
+      />
+      <span onClick={togglePasswordVisibility}>
+        {showPassword ? 'Hide' : 'Show'}
+      </span>
       <button>Masuk</button>
     </form>
   );
