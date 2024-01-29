e050f4925746aaa7b98bc177ba07ab0916201ab9
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-01-29 12:21:46 +0700

    added context theme in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,18 @@ import NotFoundPage from './pages/NotFoundPage';
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import { useAuth } from './hooks/useContext';
+import { useAuth, useTheme } from './hooks/useContext';
 
 function App() {
   const { initializePage } = useAuth();
-  if (initializePage) return;
+  const { theme } = useTheme();
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, []);
   
+  if (initializePage) return;
+
   return (
     <div className='app'>
       <header className='app__header'>
